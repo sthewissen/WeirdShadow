49dfd6363d1db06f8680ed2bb46dec211da64001
sthewissen <github@thewissen.io>
2019-09-06 11:12:07 +0200

    Added initial commit

---
+++ WeirdShadow.Android/obj/Debug/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 1c5cf7b749832ab89ecbbb76ea09a709ab41f600 */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1345
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 352391
+	.include	"typemap.jm.inc"
---
+++ WeirdShadow.Android/obj/Debug/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 1c5cf7b749832ab89ecbbb76ea09a709ab41f600 */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1345
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 352391
+	.include	"typemap.jm.inc"
